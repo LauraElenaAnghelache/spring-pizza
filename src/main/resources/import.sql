d70b9fbe5507c2f365929dcfa4adcd9eb96b4c74
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-07-29 03:09:18 +0200

    lista pizze

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `spring-pizza`.pizza (descrizione, nome, prezzo) VALUES('Pizza classica con pomodoro e mozzarella', ' Margherita', 4);
+INSERT INTO `spring-pizza`.pizza (descrizione, nome, prezzo) VALUES('Pizza con mozzarella, funghi e salsicia', ' Boscaiola base bianca', 6);
+INSERT INTO `spring-pizza`.pizza (descrizione, nome, prezzo) VALUES('Pizza con prosciutto cotto e mozzarella', 'Crostin', 6);
